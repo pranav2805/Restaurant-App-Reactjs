d8aae9f7870db23abf0f03582ae4376f1c04a76d
pranav2805 <pranavpradeep095@gmail.com>
2023-10-09 20:45:21 +0530

    Added header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h2>ReactMeals</h2>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Food items" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
